bac27e029d23e046da7b9e98626e246e138b7254
techj <techj@LAPTOP-7LLVGT64>
2017-02-27 08:02:02 -0500

    Add project files.

---
+++ CIT280-Capstone/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CIT280_Capstone.MvcApplication" Language="C#" %>
---
+++ CIT280-Capstone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIT280_Capstone.Startup))]
+namespace CIT280_Capstone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
