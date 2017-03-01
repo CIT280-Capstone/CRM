bff85be065d00fa669a32bcf0771c429c3405d79
techjes <techjes@gmail.com>
2017-03-01 09:23:24 -0500

    Repo Interface and initial controllers created

---
+++ CIT280-Capstone/DAL/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CIT280_Capstone.Models;
+
+namespace CIT280_Capstone.DAL
+{
+    public interface IRepository
+    {
+        IEnumerable<Customer> SelectCustomers();
+        Customer SelectCustomer(int customerID);
+        void EditCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
+
+        IEnumerable<Order> SelectOrders();
+        Order SelectOrder(int orderID);
+
+
+        IEnumerable<Promotion> SelectPromotions();
+        Promotion SelectPromotion(int promotionId);
+        void EditPromotion(Promotion promotion);
+    }
+}
---
+++ CIT280-Capstone/Models/Promotion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIT280_Capstone.Models
+{
+    public class Promotion
+    {
+        
+    }
+}
