14cb92e6fe67e1bbcbdce841d9c29efb7f91faa8
mdodson1995 <m.dodson@hotmail.com>
2017-03-13 08:23:51 -0400

    Added to customer model

--- CIT280-Capstone/Models/Customer.cs
+++ CIT280-Capstone/Models/Customer.cs
@@ -8,6 +8,19 @@ namespace CIT280_Capstone.Models
 {
     public class Customer
     {
-        
+        public string CFN { get; set; }
+        //customer first name
+        public string CLN { get; set; }
+        //customer last name
+        public string StrAddress { get; set; }
+        //street address
+        public string City { get; set; }
+        public int ZipCode { get; set; }
+        public int PhoneNumber { get; set; }
+        public string email { get; set; }
+        public bool RecievePromotions { get; set; }
+        //Ask if they would like to recieve promotions
+
+
     }
 }
